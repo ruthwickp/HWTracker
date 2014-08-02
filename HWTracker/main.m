6c1c8b9b83b4ef50782ed3b326d7b0cd6528c2b5
ruthwickp <ruthwickp@yahoo.com>
2014-08-02 13:59:51 -0700

    Initial Commit

---
+++ HWTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HWTracker
+//
+//  Created by Ruthwick Pathireddy on 8/2/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
