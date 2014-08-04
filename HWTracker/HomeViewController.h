e04736b3c5b7ee4c12523e2e44921118b66677d9
ruthwickp <ruthwickp@yahoo.com>
2014-08-03 20:07:18 -0700

    Created home page view

---
+++ HWTracker/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  HWTracker
+//
+//  Created by Ruthwick Pathireddy on 8/3/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// View controller for login page
+@interface HomeViewController : UIViewController
+@end
---
+++ HWTracker/HomeViewController.m
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.m
+//  HWTracker
+//
+//  Created by Ruthwick Pathireddy on 8/3/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Removes keyboard when tapped
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeKeyboard)];
+    [self.view addGestureRecognizer:tap];
+}
+
+// Removes keyboard from view
+- (void)removeKeyboard
+{
+    [self.view endEditing:YES];
+}
+
+
+@end
