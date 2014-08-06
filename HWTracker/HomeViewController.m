284f19f1b3d3160d353304d091366c2cfcdf0c00
ruthwickp <ruthwickp@yahoo.com>
2014-08-06 16:26:02 -0700

    Created login button functionality

--- HWTracker/HomeViewController.m
+++ HWTracker/HomeViewController.m
@@ -7,13 +7,31 @@
 //
 
 #import "HomeViewController.h"
+#import "DatabaseAvailability.h"
+#import <CoreData/CoreData.h>
+#import "Student.h"
+#import "Teacher.h"
 
 @interface HomeViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextView *errorTextView;
 @end
 
 @implementation HomeViewController
+
+#pragma mark - View Controller Lifecycle
+
+// Makes the view controller listen for context notifications
+- (void)awakeFromNib
+{
+    [[NSNotificationCenter defaultCenter] addObserverForName:DatabaseAvailabilityNotification
+                                                      object:self
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification *note) {
+                                                      self.context = note.userInfo[DatabaseContext];
+                                                  }];
+}
 
 - (void)viewDidLoad
 {
@@ -30,18 +48,79 @@
     [self.view endEditing:YES];
 }
 
-// Determines whether of not to login when button is pressed
-- (IBAction)loginButtonPressed
-{
-    if ([self validLoginUsernameAndPassword]) {
-        
+#pragma mark - Button Presses
+
+// Determines whether or not to login when button is pressed
+- (IBAction)loginButtonPressed:(UIButton *)sender {
+    // Sees if a student logged in and segues
+    Student *student = [self studentLogin];
+    if (student) {
+        [self performSegueWithIdentifier:@"Student Login" sender:sender];
+        return;
     }
+    
+    // Checks if teacher logged in and segues
+    Teacher *teacher = [self teacherLogin];
+    if (teacher) {
+        [self performSegueWithIdentifier:@"Teacher Login" sender:sender];
+        return;
+    }
+    
+    // If there is no match, display error message in textview
+    NSDictionary *attributes = @{ NSFontAttributeName : [UIFont boldSystemFontOfSize:[UIFont systemFontSize]],
+                                  NSForegroundColorAttributeName : [UIColor redColor] };
+    NSAttributedString *errorMessage = [[NSAttributedString alloc] initWithString:@"Invalid username or password"
+                                                                       attributes:attributes];
+    self.errorTextView.attributedText = errorMessage;
+    self.errorTextView.textAlignment = NSTextAlignmentCenter;
+
 }
 
-// Returns whether of not the user name and password are valid
-- (BOOL)validLoginUsernameAndPassword
+
+// Returns a Student with the given username and password
+- (Student *)studentLogin
 {
+    Student *student = nil;
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
     
+    // Makes a request to see if a student logged in
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Student"];
+    request.predicate = [NSPredicate predicateWithFormat:@"(username = %@) AND (password = %@)", username, password];
+    
+    // Determines if there is a match for student
+    NSError *error;
+    NSArray *matches = [self.context executeFetchRequest:request error:&error];
+    if (error || !matches || [matches count] > 1) {
+        NSLog(@"Error in retrieving login match for student");
+    }
+    else if ([matches count]) {
+        student = [matches firstObject];
+    }
+    return student;
+}
+
+// Returns whether the teacher login was valid
+- (Teacher *)teacherLogin
+{
+    Teacher *teacher = nil;
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    // Makes a request to see if a teacher logged in
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Teacher"];
+    request.predicate = [NSPredicate predicateWithFormat:@"(username = %@) AND (password = %@)", username, password];
+    
+    // Determines if there is a match for teacher
+    NSError *error;
+    NSArray *matches = [self.context executeFetchRequest:request error:&error];
+    if (error || !matches || [matches count] > 1) {
+        NSLog(@"Error in retrieving login match for teacher");
+    }
+    else if ([matches count]) {
+        teacher = [matches firstObject];
+    }
+    return teacher;
 }
 
 @end
